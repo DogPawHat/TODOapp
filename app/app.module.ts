dd24a0d458be1e2f3b53f4bee7090a4a6cea11ae
DogPawHat <ciaran.curley.11.11@gmail.com>
2015-05-12 12:18:05 +0100

    Still developing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,1 +1,14 @@
 ﻿/// <reference path="app.bundle.ts" />
+module TODOApp {
+    'use strict';
+
+    export var TODOAppModule: ng.IModule =
+        angular.module('TODOApp',
+            [
+                "ui.router",
+                "infinite-scroll",
+                "angular-locker",
+                "ngMessages"
+            ])
+    ;
+}
