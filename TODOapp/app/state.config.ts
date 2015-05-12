c8d3fa682d07e76358077d9a631656b747ade8b4
DogPawHat <ciaran.curley.11.11@gmail.com>
2015-05-12 12:18:05 +0100

    Still developing

--- TODOapp/app/state.config.ts
+++ TODOapp/app/state.config.ts
@@ -1,2 +1,32 @@
-﻿body {
+﻿module TODOApp {
+    'use strict';
+
+    export var stateNames = {
+        main: "main",
+        newThing: "newThing",
+    }
+
+    export interface statesObject {
+        main: ng.ui.IState;
+        newThing: ng.ui.IState;
+    }
+
+    export var states: statesObject = {
+        main: {
+            name: stateNames.main,
+            templateUrl: "areas/main/main.html",
+            url: ''
+        },
+        newThing: {
+            name: stateNames.newThing,
+            templateUrl: "areas/newThing/newThing.html",
+            url: '/add'
+        }
+    }
+
+
+    TODOAppModule.config(($stateProvider: ng.ui.IStateProvider) => {
+        $stateProvider.state(states.main).state(states.newThing);
+    });
+
 }
