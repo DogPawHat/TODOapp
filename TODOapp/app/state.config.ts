b340842431ac2f3e9f16fa41dda2ed39efa0a5d0
DogPawHat <ciaran.curley.11.11@gmail.com>
2015-05-15 15:51:21 +0100

    Added floating layout to add page.

--- TODOapp/app/state.config.ts
+++ TODOapp/app/state.config.ts
@@ -10,22 +10,37 @@ module TODOApp {
     export var states: IStatesObject = {
         main: {
             name: stateNames.main,
-            templateUrl: "/app/areas/main/main.html",
-            controller: "MainCtrl",
-            controllerAs: "mainCtrl",
-            url: ''
+            views: {
+                header: {
+                    templateUrl: "/app/areas/main/mainHeader.html",
+                },
+                content: {
+                    templateUrl: "/app/areas/main/mainContent.html",
+                    controller: "MainCtrl",
+                    controllerAs: "mainCtrl"
+                }
+            },
+            url: '/'
         },
         newThing: {
             name: stateNames.newThing,
-            templateUrl: "/app/areas/newThing/newThing.html",
-            controller: "NewThingCtrl",
-            controllerAs: "newThingCtrl",
+            views: {
+                header: {
+                    templateUrl: "/app/areas/newThing/newThingHeader.html",
+                },
+                content: {
+                    templateUrl: "/app/areas/newThing/newThingContent.html",
+                    controller: "NewThingCtrl",
+                    controllerAs: "newThingCtrl"
+                }
+            },
             url: '/add'
         }
     }
 
     TODOAppModule.config(($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) => {
         $stateProvider.state(states.main).state(states.newThing);
+        $urlRouterProvider.otherwise('/');
     });
 
 }
